a0f168f977118a94b90dd4609ce5caf99615864d
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2015-04-30 14:41:57 +0200

    portal: Split base template

--- hades_portal/views.py
+++ hades_portal/views.py
@@ -1,4 +1,5 @@
 from flask import request, render_template
+from flask_babel import _
 import arpreq
 from hades_portal import app
 from hades_portal.db import get_groups, get_latest_auth_attempt
@@ -8,13 +9,23 @@ def index():
     try:
         mac = arpreq.arpreq(request.remote_addr)
     except arpreq.ARPError as e:
-        return render_template("base.html", error=True)
+        content = render_template("error.html",
+                                  message=_("Could not determine your MAC "
+                                            "address"))
+        return content, 500
     if mac is None:
-        return render_template("base.html", error=True)
+        content = render_template("error.html",
+                                  error=_("Could not determine your MAC "
+                                          "address."))
+        return content, 500
     mac_groups = get_groups(mac)
-    last_auth_groups, last_auth_date = get_latest_auth_attempt(mac)
+    latest_auth_attempt = get_latest_auth_attempt(mac)
+    if latest_auth_attempt:
+        last_auth_groups, last_auth_date = latest_auth_attempt
+    else:
+        last_auth_groups, last_auth_date = [], None
     return render_template(
-        "base.html",
+        "status.html",
         violation='violation' in mac_groups,
         security='security' in mac_groups,
         default_in_payment='default_in_payment' in mac_groups,
@@ -22,8 +33,6 @@ def index():
         unknown=not mac_groups,
         wrong_port='unknown' in last_auth_groups,
         membership_ended='membership_ended' in mac_groups,
-        last_auth_groups=last_auth_groups,
-        last_auth_date=last_auth_date,
         mac=mac,
     )
 
