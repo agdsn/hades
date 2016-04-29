80f77b0a1814728fec72431dd2e316851cc1f1b7
Stefan Haller <stefan.haller@tu-dresden.de>
2016-04-29 18:14:43 +0200

    Improve docker/postgres_fdw script
      - use `python3 -m hades.common.su` instead of `su postgres`
      - fix typo in comment

--- docker/postgres_fdw.sh
+++ docker/postgres_fdw.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "$(id -u)" -eq 0 ]]; then
-    su postgres "$0" "$@"
-    exit "$?"
-fi
-
 . /etc/hades/env
 export PATH="/usr/lib/postgresql/${PGVERSION}/bin:${PATH}"
 export PGDATA="/var/lib/postgresql/hades"
+source <(python3 -m hades.config.export)
+
+# Run as postgres user
+if [[ $(id -u) -eq 0 ]]; then
+    exec python3 -m hades.common.su "${HADES_POSTGRESQL_USER}" "$0" "$@"
+fi
 
 # Let the hades script configure the database cluster. This might use some
 # special arguments to pg_ctl initdb.
-hades init-database-cluster
+hades init-postgres-cluster
 
-# Create database 'radius' and populate the schema. This enables us point the
-# foreign tables to the local running database instance for development.
+# Create database 'radius' and populate the schema. This enables us to point
+# the foreign tables to the local running database instance for development.
 pg_ctl start -w -s
 createdb radius
 python3 -m hades.config.generate schema_fdw.sql.j2 | psql --quiet --set=ON_ERROR_STOP=1 --no-psqlrc --single-transaction --file=- radius
