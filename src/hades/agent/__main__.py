51845a84be71a2b3e579fa1094a7cd7c33264cda
Stefan Haller <stefan.haller@tu-dresden.de>
2016-05-24 00:17:28 +0200

    Commit transactions in hades agent
    Previously all the changes were discarded, because the transactions were
    not committed correctly.

--- src/hades/agent/__main__.py
+++ src/hades/agent/__main__.py
@@ -12,25 +12,27 @@ logger = logging.getLogger(__name__)
 def refresh():
     logger.info("Refreshing materialized views")
     connection = get_connection()
-    # TODO: After updating the nas table, we have to restart (reload?)
-    # the freeradius server. Currently, this must be done manually.
-    connection.execute("REFRESH MATERIALIZED VIEW nas")
-    connection.execute("REFRESH MATERIALIZED VIEW radcheck")
-    connection.execute("REFRESH MATERIALIZED VIEW radgroupcheck")
-    connection.execute("REFRESH MATERIALIZED VIEW radgroupreply")
-    connection.execute("REFRESH MATERIALIZED VIEW radusergroup")
+    with connection.begin() as trans:
+        # TODO: After updating the nas table, we have to restart (reload?)
+        # the freeradius server. Currently, this must be done manually.
+        connection.execute("REFRESH MATERIALIZED VIEW nas")
+        connection.execute("REFRESH MATERIALIZED VIEW radcheck")
+        connection.execute("REFRESH MATERIALIZED VIEW radgroupcheck")
+        connection.execute("REFRESH MATERIALIZED VIEW radgroupreply")
+        connection.execute("REFRESH MATERIALIZED VIEW radusergroup")
 
 
 def delete_old():
     logger.info("Deleting old records")
     conf = get_config()
     connection = get_connection()
-    result = connection.execute(radacct.delete().where(and_(
-        radacct.c.acctstoptime < utcnow() - conf["HADES_RETENTION_INTERVAL"]
-    )))
-    result = connection.execute(radpostauth.delete().where(and_(
-        radpostauth.c.authdate < utcnow() - timedelta(days=1)
-    )))
+    with connection.begin() as trans:
+        result = connection.execute(radacct.delete().where(and_(
+            radacct.c.acctstoptime < utcnow() - conf["HADES_RETENTION_INTERVAL"]
+        )))
+        result = connection.execute(radpostauth.delete().where(and_(
+            radpostauth.c.authdate < utcnow() - timedelta(days=1)
+        )))
 
 
 def get_sessions(self, mac):
