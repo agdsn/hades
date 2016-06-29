5ec52156c2ac1070a241d58a2b52eaf46ed86e8c
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2016-06-29 02:09:55 +0200

    Add support for different config variable export formats
    Adds support for POSIX shell, systemd EnvironmentFile, bash, ksh and zsh.

--- src/hades/config/export.py
+++ src/hades/config/export.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import logging
 import os
@@ -26,25 +27,39 @@ def escape(value):
 def main():
     parser = ArgumentParser(description='Export options as shell '
                                                  'variables',
+                            epilog='Python sequence and mapping types will '
+                                   'only be exported, if the destination '
+                                   'format support it',
                             parents=[parent_parser])
+    parser.add_argument('--format', choices=('systemd', 'posix', 'bash', 'ksh',
+                                             'zsh'),
+                        default='systemd', help='Export format.')
+    parser.add_argument('file', type=argparse.FileType('wb'), metavar='FILE',
+                        default='-', nargs='?',
+                        help='Output destination (default: stdout)')
     args = parser.parse_args()
     config = load_config(args.config)
+    mappings = args.format in ('bash', 'ksh', 'zsh')
+    sequences = args.format in ('bash', 'ksh', 'zsh')
     for name, value in config.items():
-        escaped_name = escape(str(name))
+        name = escape(name)
         if isinstance(value, shell_types):
-            print("{}={}".format(escaped_name, escape(value)))
-        elif isinstance(value, collections.Mapping):
-            print("declare -A {}".format(name))
-            for k, v in value.items():
-                if isinstance(v, shell_types):
-                    print("{}[{}]={}".format(escaped_name, escape(str(k)),
-                                             escape(str(v))))
-        elif isinstance(value, collections.Sequence):
-            print("declare -a {}".format(name))
-            for index, v in enumerate(value):
-                if isinstance(v, shell_types):
-                    print("{}[{}]={}".format(escaped_name, index,
-                                             escape(str(v))))
+            print("{}={}".format(name, escape(value)), file=args.file)
+        elif isinstance(value, collections.Mapping) and mappings:
+            if args.format == 'bash':
+                print("declare -A {}".format(name), file=args.file)
+            if args.format in ('ksh', 'zsh'):
+                print("typeset -A {}".format(name), file=args.file)
+            value = ' '.join("[{}]={}".format(escape(k), escape(v))
+                             for k, v in value.items()
+                             if isinstance(k, shell_types) and
+                             isinstance(v, shell_types))
+            print("{}=({})".format(name, value), file=args.file)
+        elif isinstance(value, collections.Sequence) and sequences:
+            value = ' '.join(escape(v) for v in value
+                             if isinstance(v, shell_types))
+            print("{}=({})".format(name, value), file=args.file)
+    return os.EX_OK
 
 
 if __name__ == '__main__':
