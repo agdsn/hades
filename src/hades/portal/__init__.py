b4d82c21d4645d3a7a4e169813797eced5478d7a
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2016-07-09 02:27:36 +0200

    Add a NullSession
    We don't want to use cookies or any other client storage facilities. To get an
    appropriate error message, add a NullSession that will raise an error if the
    session is used to store any information.
    
    Reading information from the session still works, but won't return any data.

--- src/hades/portal/__init__.py
+++ src/hades/portal/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask.ext.babel import Babel
 
+from hades.portal.session import NullSessionInterface
+
 app = Flask(__name__)
+app.session_interface = NullSessionInterface()
 babel = Babel(app)
---
+++ src/hades/portal/session.py
@@ -0,0 +1,30 @@
+import collections
+from flask.sessions import SessionInterface, SessionMixin
+
+
+class NullSession(collections.MutableMapping, SessionMixin):
+    """
+    A session similar to the Flask's :class:`flask.sessions.NullSession`, but
+    with a different error message.
+    """
+    def __len__(self):
+        return 0
+
+    def __getitem__(self, key):
+        raise KeyError(key)
+
+    def __iter__(self):
+        return iter(())
+
+    def _fail(self, *args, **kwargs):
+        raise RuntimeError("Sessions are not supported.")
+
+    __delitem__ = __setitem__ = _fail
+
+
+class NullSessionInterface(SessionInterface):
+    def open_session(self, app, request):
+        return NullSession()
+
+    def save_session(self, app, session, response):
+        pass
