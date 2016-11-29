371db5f959f26f844334a4fdced7f77190cd3ad9
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2016-11-30 00:13:40 +0100

    Move assets and portal templates back into package

--- src/hades/portal/__init__.py
+++ src/hades/portal/__init__.py
@@ -1,16 +1,14 @@
-import os.path
-
 from flask import Flask
 from flask_babel import Babel
+from pkg_resources import resource_filename
 
-from hades import constants
 from hades.portal.session import NullSessionInterface
 
 app = Flask(
     __name__,
     static_url_path='assets',
-    template_folder=os.path.join(constants.templatedir, 'portal'),
-    static_folder=constants.assetdir,
+    template_folder=resource_filename(__package__, 'templates'),
+    static_folder=resource_filename(__package__, 'assets'),
 )
 app.session_interface = NullSessionInterface()
 babel = Babel(app)
