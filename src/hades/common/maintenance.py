e6756b8b05c92ad06b174667a0f038b9a1d2ace1
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2017-04-30 18:09:35 +0200

    Implement a DBus deputy service for privileged operations
    Some maintenance operations require a privileged user, namely
    - to reload the auth DHCP hosts a signal must be sent to the
      hades-auth-dhcp.service
    - to reload the RADIUS clients a signal must be sent to the
      hades-radius.service
    - to reload the ipset for alternative DNS clients, ipset must be called
    
    Instead of running the hades-agent.service and the hades-refresh.service as
    root or with sufficient elevated capabilities, we run a small daemon that
    provides a DBus service instead. The daemon will read the information from the
    database, write new configuration files and instruct systemd to reload the
    services.

--- src/hades/common/maintenance.py
+++ src/hades/common/maintenance.py
@@ -2,11 +2,8 @@
 Common maintenance functionality
 """
 import logging
-import os
-import signal
-import subprocess
 
-import netaddr
+from pydbus import SystemBus
 from sqlalchemy import null
 
 import hades.common.db as db
@@ -16,75 +13,22 @@ from hades.config.loader import get_config
 logger = logging.getLogger(__name__)
 
 
-def sighup_from_pid_file(pid_file):
-    try:
-        with open(pid_file, mode='rb') as f:
-            data = f.readline()
-    except OSError as e:
-        logger.error("Could not read PID file %s: %s", pid_file, e.strerror)
-        return
-    try:
-        pid = int(data)
-    except ValueError:
-        logger.error("Invalid PID in PID file %s: %s", pid_file, data)
-        return
-    if pid < 1:
-        logger.error("Invalid PID in PID file %s: %d", pid_file, pid)
-    try:
-        os.kill(pid, signal.SIGHUP)
-    except OSError as e:
-        logger.error("Can't send SIGHUP to pid %d from PID file %s: %s", pid,
-                     pid_file, e.strerror)
+def signal_auth_dhcp_hosts_reload():
+    bus = SystemBus()
+    deputy = bus.get(constants.DEPUTY_DBUS_NAME)
+    deputy.ReloadAuthDhcpHosts()
 
 
-def reload_auth_dnsmasq():
-    sighup_from_pid_file(constants.AUTH_DHCP_PID_FILE)
+def signal_radius_clients_reload():
+    bus = SystemBus()
+    deputy = bus.get(constants.DEPUTY_DBUS_NAME)
+    deputy.ReloadRadiusClients()
 
 
-def reload_freeradius():
-    sighup_from_pid_file(constants.RADIUS_PID_FILE)
-
-
-def generate_dhcp_host_reservations(hosts):
-    for mac, ip in hosts:
-        try:
-            mac = netaddr.EUI(mac, dialect=netaddr.mac_unix_expanded)
-        except netaddr.AddrFormatError:
-            logger.error("Invalid MAC address %s", mac)
-            continue
-        try:
-            ip = netaddr.IPAddress(ip)
-        except netaddr.AddrFormatError:
-            logger.error("Invalid IP address %s", ip)
-            continue
-        yield "{0},{1}\n".format(mac, ip)
-
-
-def generate_dhcp_hosts_file():
-    file_name = constants.AUTH_DHCP_HOSTS_FILE
-    logger.info("Generating DHCP hosts file %s", file_name)
-    hosts = db.get_all_dhcp_hosts()
-    try:
-        with open(file_name, mode='w', encoding='ascii') as f:
-            f.writelines(generate_dhcp_host_reservations(hosts))
-    except OSError as e:
-        logger.error("Error writing %s: %s", file_name, e.strerror)
-
-
-def update_alternative_dns_ipset():
-    conf = get_config()
-    ipset_name = conf['HADES_AUTH_DNS_ALTERNATIVE_IPSET']
-    tmp_ipset_name = 'tmp_' + ipset_name
-    logger.info("Updating alternative_dns ipset (%s)", ipset_name)
-    tmp = []
-    tmp.append('create {} hash:ip -exist'.format(tmp_ipset_name))
-    tmp.append('flush {}'.format(tmp_ipset_name))
-    for ip in db.get_all_alternative_dns_ips():
-        tmp.append('add {} {}'.format(tmp_ipset_name, ip))
-    tmp.append('swap {} {}'.format(ipset_name, tmp_ipset_name))
-    tmp.append('destroy {}'.format(tmp_ipset_name))
-    subprocess.run([constants.IP, 'netns', 'exec', 'auth', constants.IPSET, 'restore'],
-                   input='\n'.join(tmp).encode('ascii'))
+def signal_alternative_auth_dns_clients_reload():
+    bus = SystemBus()
+    deputy = bus.get(constants.DEPUTY_DBUS_NAME)
+    deputy.ReloadAlternativeAuthDnsClients()
 
 
 def refresh():
@@ -93,16 +37,18 @@ def refresh():
     result = db.refresh_and_diff_materialized_view(connection, db.dhcphost,
                                                    db.temp_dhcphost, [null()])
     if result != ([], [], []):
-        generate_dhcp_hosts_file()
-        reload_auth_dnsmasq()
+        logger.info('DHCP host reservations changed. Signaling reload.')
+        signal_auth_dhcp_hosts_reload()
     result = db.refresh_and_diff_materialized_view(connection, db.nas,
                                                    db.temp_nas, [null()])
     if result != ([], [], []):
-        reload_freeradius()
+        logger.info('RADIUS clients changed. Signaling reload.')
+        signal_radius_clients_reload()
     result = db.refresh_and_diff_materialized_view(connection, db.alternative_dns,
                                                    db.temp_alternative_dns, [null()])
     if result != ([], [], []):
-        update_alternative_dns_ipset()
+        logger.info('Alternative auth DNS clients changed. Signaling reload.')
+        signal_alternative_auth_dns_clients_reload()
     db.refresh_materialized_view(connection, db.radcheck)
     db.refresh_materialized_view(connection, db.radgroupcheck)
     db.refresh_materialized_view(connection, db.radgroupreply)
