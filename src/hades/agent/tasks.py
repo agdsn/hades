af999e86bb3dba6cd8aee048877b2fbbecbeeee9
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2017-11-09 03:08:54 +0100

    Add release_dhcp_lease Celery API
    Fixes #67

--- src/hades/agent/tasks.py
+++ src/hades/agent/tasks.py
@@ -22,7 +22,11 @@ from hades.common.db import (
     get_sessions_of_mac as do_get_sessions_of_mac,
 )
 from hades.config.loader import get_config
-from hades.deputy.client import signal_cleanup, signal_refresh
+from hades.deputy.client import (
+    signal_cleanup,
+    signal_auth_dhcp_lease_release,
+    signal_refresh,
+)
 
 logger = get_task_logger(__name__)
 engine: Optional[Engine] = None
@@ -194,6 +198,12 @@ def refresh():
 def cleanup():
     """Perform a database cleanup"""
     signal_cleanup()
+
+
+@rpc_task()
+def release_auth_dhcp_lease(ip: str):
+    ip = check_ip_address("ip", ip)
+    signal_auth_dhcp_lease_release(ip)
 
 
 @rpc_task()
