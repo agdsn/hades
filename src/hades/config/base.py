5daf37ff3e04fbabc1ad3c1e0209eb6ad01a0887
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2020-01-14 13:57:22 +0100

    Move OptionDescriptor classes down
    This allows annotations in OptionDescriptor methods to reference the
    Option classes.

--- src/hades/config/base.py
+++ src/hades/config/base.py
@@ -84,6 +84,41 @@ class OptionMeta(type):
             self.runtime_check(config, value)
 
 
+class Option(object, metaclass=OptionMeta, abstract=True):
+    has_default = False
+    required = False
+    default = None
+    type = None
+    runtime_check = None
+    static_check = None
+
+
+class ConfigError(Exception):
+    """Base class for all config related errors."""
+
+
+class ConfigOptionError(ConfigError):
+    """Base class for errors related to processing a specific option"""
+    def __init__(self, *args, option: str):
+        super(ConfigOptionError, self).__init__(*args)
+        self.option = option
+
+
+class MissingOptionError(ConfigOptionError):
+    """Indicates that a required option is missing"""
+
+
+class OptionCheckError(ConfigOptionError):
+    """Indicates that an option check failed"""
+
+
+def coerce(value):
+    if isinstance(value, type) and issubclass(value, Option):
+        return value.__name__
+    else:
+        return value
+
+
 class OptionDescriptor:
     @classmethod
     def decorate(cls, f):
@@ -151,38 +186,3 @@ class Compute(OptionDescriptor):
         :raises OptionCheckError: if the value can't be computed
         """
         raise NotImplemented()
-
-
-class Option(object, metaclass=OptionMeta, abstract=True):
-    has_default = False
-    required = False
-    default = None
-    type = None
-    runtime_check = None
-    static_check = None
-
-
-class ConfigError(Exception):
-    """Base class for all config related errors."""
-
-
-class ConfigOptionError(ConfigError):
-    """Base class for errors related to processing a specific option"""
-    def __init__(self, *args, option: str):
-        super(ConfigOptionError, self).__init__(*args)
-        self.option = option
-
-
-class MissingOptionError(ConfigOptionError):
-    """Indicates that a required option is missing"""
-
-
-class OptionCheckError(ConfigOptionError):
-    """Indicates that an option check failed"""
-
-
-def coerce(value):
-    if isinstance(value, type) and issubclass(value, Option):
-        return value.__name__
-    else:
-        return value
