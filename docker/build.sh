61edfbe39bc69476a34d4f55bc229db2286c6fb5
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2021-11-27 18:54:29 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,34 +1,15 @@
 #!/usr/bin/env bash
 
-set -exuo pipefail
+set -euo pipefail
 shopt -qs failglob
 
-declare -A PACKAGES=(
-	[dh-virtualenv]=dh-virtualenv
-	[isc-kea]="kea-common kea-admin kea-dhcp4-server kea-dhcp6-server"
-	[postgresql-mysql-fdw]=postgresql-9.4-mysql-fdw
-	)
-declare -A REPOSITORIES=(
-	[dh-virtualenv]=https://github.com/spotify/dh-virtualenv.git
-	[isc-kea]=https://github.com/sebschrader/debian-pkg-isc-kea
-	[postgresql-mysql-fdw]=https://github.com/sebschrader/debian-pkg-postgresql-mysql-fdw
+readonly SOURCE_DIR="/build/hades"
+readonly -A PACKAGES=(
+	[arpreq]="${SOURCE_DIR}/vendor/arpreq"
+	[hades]="${SOURCE_DIR}"
 )
 
-pushd /build
-
-for source_package in "${!PACKAGES[@]}"; do
-	echo "Building $source_package"
-	if [[ ! -e ${source_package} ]]; then
-		git clone "${REPOSITORIES[$source_package]}" ${source_package}
-	fi
-	sudo mk-build-deps --install --remove -t 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -t jessie-backports' "${source_package}/debian/control"
-	pushd "$source_package"
-	version="$(dpkg-parsechangelog -S Version)"
-	dpkg-buildpackage -uc -us -b
-	popd
-	for package in ${PACKAGES[$source_package]}; do
-		sudo dpkg -i ${package}_${version}_*.deb
-	done
-	sudo apt-get purge "${source_package}-build-deps"
-	sudo apt-get autoremove --purge
+for package in "${!PACKAGES[@]}"; do
+	echo "Building ${package} package ..."
+	(set -x; cd "${PACKAGES[${package}]}" && dpkg-buildpackage --no-sign -b)
 done
